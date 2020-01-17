2a4ce32b55e743b05fc48b9b33269dbae28e237e
nhalflants <natacha_halflants@hotmail.com>
2020-01-18 00:30:33 +0100

    Project setup

--- Startup.cs
+++ Startup.cs
@@ -2,10 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CourseLibrary.API.DbContexts;
+using CourseLibrary.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +29,20 @@ namespace CourseLibrary.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddScoped<ICourseLibraryRepository, CourseLibraryRepository>();
+            
+            var server = Configuration["DbConnectionString:DbServer"] ?? "localhost";
+            var port = Configuration["DbConnectionString:DbPort"] ?? "1433";
+            var database = Configuration["DbConnectionString:Database"] ?? "CourseLibraryDb";
+            var user = Configuration["DbConnectionString:DbUser"] ?? "sa";
+            var password = Configuration["DbConnectionString:DbPassword"] ?? "Passw0rd";
+
+            var connectionString = $"Server={server},{port};Initial Catalog={database};User ID={user};Password={password}";
+
+            services.AddDbContext<CourseLibraryContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
